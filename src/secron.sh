2f3c16f727857858cd0c17516fd2549fc14c204b
Abhijith S <abhijith.s4395@gmail.com>
2021-01-10 15:20:47 +0530

    Update secron.sh

--- src/secron.sh
+++ src/secron.sh
@@ -58,9 +58,11 @@ else
 	echo "Enter a valid number among the following [5,10,15,20,30]"
 	echo "Make sure that you entered the second input option in qoutes 'command' "
 	echo "Please verify your syntax is correct"
-	echo -e "\nSee the Examples given Below"
-	echo -e "\n secron 10 'ping -c4 127.0.0.1'  //for running a ping job in a 10 second time interval"
-	echo " secron 30 'rsync -avz /home/testing/ /test/'  //this will set an rsync job for testing in every 30 seconds"
-	echo " * * * * * secron 20 'echo HELLO' >> hello.txt 2>&1   //adding this to crontab will set a cronjob to echo HELLO to hello.txt file in every 20 seconds"
+	echo "See the Examples given Below"
+	echo "secron 10 'ping -c4 127.0.0.1'  //for running a ping job in a 10 second time interval"
+	echo ""
+	echo "secron 30 'rsync -avz /home/testing/ /test/'  //this will set an rsync job for testing in every 30 seconds"
+	echo ""
+	echo "* * * * * secron 20 'echo HELLO' >> hello.txt 2>&1   //adding this to crontab will set a cronjob to echo HELLO to hello.txt file in every 20 seconds"
 
 fi
