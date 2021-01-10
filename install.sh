19c8f72ba4ec961d9bae172b4cbb9493870224e7
Abhijith S <abhijith.s4395@gmail.com>
2021-01-10 13:27:15 +0530

    Create install.sh
    For easy installation/test_run

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "Happy that you are here"
+cat README.md
+echo "Choose how you want to install"
+echo "Enter 1 to install the binary and run anywhere from your PC"
+echo "Enter 2 to run this as script from /home/$USER/secron.sh"
+echo "Enter 3 to test the script"
+
+read input
+
+
+if [ $input -eq 1 ]
+then
+  sudo cp bin/secron /usr/bin/
+  echo -e "\nInstalled successfully\nTry out..!!\n"
+  cat doc/how-to.txt
+  echo -e "\nPing me abhi6x043 abhijith.s4395@gmail.com\n"
+
+elif [ $input -eq 2 ]
+then
+  cp src/secron.sh /home/$USER/secron.sh
+  chmod u+x /home/$USER/secron.sh
+  echo -e "\nScript is ready to run"
+  cat doc/how-to.txt
+  echo -e "\nPing me abhi6x043 abhijith.s4395@gmail.com\n"
+  
+elif [ $input -eq 3 ]
+then
+  chmod u+x src/secron.sh
+  echo "You can now run binary or script by entering the following from this directory"
+  echo -e "\n bin/secron [OPTIONS] [COMMAND in quotes]   //To run the binary"
+  echo -e "\n sh src/secron.sh [OPTIONS] [COMMAND in quotes]  //To run the script"
+  echo -e "\nAlso please read the documentations available doc/"
+  echo -e "Ping me abhi6x043 abhijith.s4395@gmail.com\n"
+    
+else
+  echo "Please enter a valid option among the following [1, 2, 3]"
+  echo "Also please read the documentations available doc/"
+  echo -e "\nPing me abhi6x043 abhijith.s4395@gmail.com\n"
+  
+fi
